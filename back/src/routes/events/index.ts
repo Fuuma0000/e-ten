693f5322fd9f08dc4afbff718d95e308a74ef017
shiorioz <ozws011.tmt@gmail.com>
2023-11-30 12:08:44 +0900

    feat: イベント一覧の取得を追加する

--- back/src/routes/events/index.ts
+++ back/src/routes/events/index.ts
@@ -1,8 +1,16 @@
 import { Request, Response, Router } from "express";
+import { PrismaClient } from "@prisma/client";
 const router: Router = Router();
+const prisma = new PrismaClient();
 
+// イベント一覧の取得
+router.get("/", async (req: Request, res: Response) => {
+  const events = await prisma.events.findMany();
+  res.json(events);
+});
 
-router.get("/:id/students", async(req: Request, res: Response) => {
+// イベント参加学生の取得
+router.get("/:id/students", async (req: Request, res: Response) => {
   res.json({ message: "events/{id}/studentsに対するルーティング" });
 });
 
