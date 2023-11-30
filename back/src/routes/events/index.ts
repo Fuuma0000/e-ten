ef9cc4c320a1d2ff8f567871a404a0119edf294b
warabimochi <ecccompsenyou@gmail.com>
2023-11-30 11:11:55 +0900

    chore:expressコンテナのポート復活する

--- back/src/routes/events/index.ts
+++ back/src/routes/events/index.ts
@@ -1,6 +1,7 @@
 import { Request, Response, Router } from "express";
 const router: Router = Router();
 
+
 router.get("/:id/students", async(req: Request, res: Response) => {
   res.json({ message: "events/{id}/studentsに対するルーティング" });
 });
--- back/src/routes/profiles/index.ts
+++ back/src/routes/profiles/index.ts
@@ -1,8 +1,34 @@
-import express from "express";
-const router = express.Router();
+import { Request, Response, Router } from "express";
+import { PrismaClient } from "@prisma/client";
 
-router.get("/:id", async (req, res) => {
+const router: Router = Router();
+
+const prisma = new PrismaClient();
+
+router.get("/:id", async (req: Request, res: Response) => {
+  const { id } = req.params;
+
+  // const temp = await prisma.courses.findUnique({
+  //   where: {
+  //     id: parseInt(id),
+  //   },
+  // });
+
+  // const temp = await prisma.courses.findMany();
+  // console.log(temp);
+
+  console.log("test");
   res.json({ message: "ルーティング確認.profile" });
 });
 
 export { router };
+
+// usersから
+// user_id.user_name.enrollment_year.graduation_year.icon_url.e-mail.is_job_hunt_completed.self_introduciton.
+// user_idをキーにusers_jobsテーブルからjobs_idを取ってくる.jobsテーブルからjobs_idを元にnameを取ってくる
+// courses_idをキーにcoursesテーブルからnameを取ってくる
+// users_idをキーにworks_data_usersからworks_idを取ってくる.
+// works_idをキーにworks_dataテーブルからname.catch_copy.
+// works_data_genresテーブルからgenres_idを取ってくる.genres_idをキーにgenresテーブルからnameを取ってくる
+// works_data_technologiesテーブルからtechnologies_idを取ってくる.technologies_idをキーにtechnologiesテーブルからnameを取ってくる
+// work_imagesテーブルからurl取ってくる
