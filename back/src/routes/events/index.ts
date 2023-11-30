688e3b486e8ffef27c9df1997841dcc3467cc751
warabimochi <ecccompsenyou@gmail.com>
2023-11-30 09:37:47 +0900

    refactor:コントローラーのひな型をTS化する

--- back/src/routes/events/index.ts
+++ back/src/routes/events/index.ts
@@ -1,8 +1,8 @@
-import express from "express";
-const router = express.Router();
+import { Request, Response, Router } from "express";
+const router: Router = Router();
 
-router.get("/:id/students", async (req, res) => {
-  res.json({ message: "ルーティング確認.events" });
+router.get("/:id/students", async(req: Request, res: Response) => {
+  res.json({ message: "events/{id}/studentsに対するルーティング" });
 });
 
 export { router };
