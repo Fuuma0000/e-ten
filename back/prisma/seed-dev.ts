352d92763b709d32d19785910b3786deb71f4f5b
warabimochi <ecccompsenyou@gmail.com>
2023-12-02 08:24:19 +0900

    feat:必要なデータをDBから拾って返せるようにする

--- back/prisma/seed-dev.ts
+++ back/prisma/seed-dev.ts
@@ -16,7 +16,151 @@ async function main() {
   });
 }
 
+
+// 混ぜると分からなくなるので関数切っておきます
+async function insertProfileSeedData() {
+  // ユーザ情報
+  const registedUser = await prisma.users.create({
+    data: {
+      email: "example@example.com",
+      password: "example",
+      username: "testuser",
+      courses_id: 1,
+      enrollment_year: 2022,
+      graduation_year: 2026,
+      is_job_hunt_completed: false,
+      self_introduction: "ここに自己紹介のテキストが入ります",
+      icon_url: "https://cdn2.thecatapi.com/images/c5b.jpg",
+    },
+  });
+
+
+  // 志望職種情報
+  const registedJob = await prisma.jobs.create({
+    data: {
+      name: "ここに志望職種の値が入ります"
+    },
+  });
+
+  // 志望職種情報とユーザ情報の中間テーブル
+  const registedUserId = registedUser.id;
+  const registedJobId = registedJob.id;
+
+  await prisma.users_jobs.create({
+    data: {
+      users_id: registedUserId,
+      jobs_id: registedJobId,
+    }
+  });
+
+  // コース情報
+  await prisma.courses.create({
+    data: {
+      name: "ここにコースの名前が入ります"
+    },
+  });
+
+  //  worksテーブル
+  const registedWork = await prisma.works.create({
+    data: {
+      events_id: 1,
+      latest_reviewed_id: null,
+    }
+  });
+  
+  // 作品情報
+  const registedWorkId = registedWork.id;
+  const registedWorkData = await prisma.works_data.create({
+    data: {
+      works_id: registedWorkId,
+      name: "ここに作品名が入ります",
+      catch_copy: "ここにキャッチコピーが入ります",
+      description: "ここに作品概要が入ります",
+      works_url: "ここに作品のURLが入ります",
+      movie_url: "ここに動画URLが入ります",
+      system_diagram_url: "ここにシステム構成図が入ります",
+      detail: "ここに作品詳細が入ります"
+    }
+  });
+
+  // nullで作ってるから参照出来るようにupdateする
+  const registedWorkDataId = registedWorkData.id;
+  const registedWorkDataWorkId = registedWorkData.works_id;
+  await prisma.works.update({
+    where: { id: registedWorkDataWorkId },
+    data: {
+      latest_reviewed_id: registedWorkDataId
+    }
+  })
+  
+
+  // 作品情報とユーザ情報の中間テーブル
+  await prisma.works_data_users.create({
+    data: {
+      works_data_id: registedWorkDataId,
+      users_id: registedUserId,
+      role_explanation: "ここに担当箇所が入ります"
+    }
+  });
+  
+
+
+  //  ジャンル情報
+  const registedGenre = await prisma.genres.create({
+    data: {
+      name: "ここにジャンル名が入ります"
+    }
+  });
+
+  // 作品情報とジャンル情報の中間テーブル
+  const registedGenreId = registedGenre.id;
+  await prisma.works_data_genres.create({
+    data: {
+      works_data_id: registedWorkDataId,
+      genres_id: registedGenreId,
+    }
+  })
+
+  // 技術情報
+  const registedTechnology = await prisma.technologies.create({
+    data: {
+      name: "ここに技術名が入ります"
+    }
+  });
+
+  // 作品情報と技術情報の中間テーブル
+  const registedTechnologyId = registedTechnology.id;
+  await prisma.works_data_technologies.create({
+    data: {
+      works_data_id: registedWorkDataId,
+      technologies_id: registedTechnologyId
+    }
+  });
+
+
+  // 作品画像情報
+  await prisma.works_data_images.create({
+    data: {
+      works_data_id: registedWorkDataId,
+      url: "ここに画像が詰まっているS3のアドレスが入ります",
+      order: 1
+    }
+  });
+}
+
+
 main()
+  .then(async () => {
+    await prisma.$disconnect();
+  })
+  .catch(async (e) => {
+    console.error(e);
+    await prisma.$disconnect();
+    process.exit(1);
+  });
+
+  //  切った関数の実行
+  insertProfileSeedData()
   .then(async () => {
     await prisma.$disconnect();
   })
--- back/src/routes/profiles/index.ts
+++ back/src/routes/profiles/index.ts
@@ -8,18 +8,101 @@ const prisma = new PrismaClient();
 router.get("/:id", async (req: Request, res: Response) => {
   const { id } = req.params;
 
+  // works_data_usersからworksを上手く辿れないので分けて記述しています
   const temp = await prisma.users.findUnique({
     where: {
       id: parseInt(id),
     },
-    include: {
-      
-    }
-  });
+    select: {
+      id: true,
+      username: true,
+      enrollment_year: true,
+      graduation_year: true,
+      icon_url: true,
+      email: true,
+      is_job_hunt_completed: true,
+      self_introduction: true,
+      users_jobs: {
+        select: {
+          jobs: {
+            select: {
+              name: true,
+            }
+          }
+        }
+      },
+      courses: {
+        select: {
+          name: true
+        }
+      },
+      // works_data_users: {
+      //   select: {
+      //     works_data: {
+      //       select: {
+      //         works: {
 
+      //         }
+      //       }
+      //     }
+      //   }
+      // }
+    },
+    });
+
+    
+    // TODO:複数個レコードが取得出来た場合を考慮していないので入っているレコードが適切かどうかチェックしてもらってから作成する
+    const temp2 = await prisma.works_data_users.findMany({
+      where: { users_id: parseInt(id) },
+      select: {
+        works_data_id: true 
+      }
+    });
+
+    const temp3 = await prisma.works_data.findUnique({
+      where: { id: temp2[0].works_data_id },
+      select: { works_id: true }
+    });
+
+    const temp4 = await prisma.works.findUnique({
+      where: { id: temp3?.works_id },
+      select: { latest_reviewed_id: true }
+    });
+
+    const temp5 = await prisma.works_data.findUnique({
+      where: { id: temp4?.latest_reviewed_id as number | undefined },
+      select: {
+        works_id: true,
+        name: true,
+        works_data_genres: {
+          select: {
+            genres: {
+              select: {
+                name: true
+              }
+            }
+          }
+        },
+        works_data_technologies: {
+          select: {
+            technologies: {
+              select: {
+                name: true
+              }
+            }
+          }
+        },
+        catch_copy: true,
+        works_data_images: {
+          select: {
+            url: true,
+          }
+        }
+      }
+    })
   console.log(temp);
 
-  res.json({ message: "ルーティング確認.profile" });
+  res.json(temp5);
 });
 
 export { router };
@@ -28,8 +111,11 @@ export { router };
 // user_id.user_name.enrollment_year.graduation_year.icon_url.e-mail.is_job_hunt_completed.self_introduciton.
 // user_idをキーにusers_jobsテーブルからjobs_idを取ってくる.jobsテーブルからjobs_idを元にnameを取ってくる
 // courses_idをキーにcoursesテーブルからnameを取ってくる
-// users_idをキーにworks_data_usersからworks_idを取ってくる.
-// works_idをキーにworks_dataテーブルからname.catch_copy.
+// users_idをキーにworks_data_usersからworks_data_idを取ってくる.
+// works_data_idをキーにworksテーブルからlatest_reviewed_idを取ってくる
+// latest_reviewed_idをキーにworks_dataテーブルからname.catch_copy.
 // works_data_genresテーブルからgenres_idを取ってくる.genres_idをキーにgenresテーブルからnameを取ってくる
 // works_data_technologiesテーブルからtechnologies_idを取ってくる.technologies_idをキーにtechnologiesテーブルからnameを取ってくる
 // work_imagesテーブルからurl取ってくる
+
+// works.id指定で検索(latest_reviewed_id).works_data
