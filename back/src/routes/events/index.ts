a6d2d8f48c31fa3ff4d8088ff0f85f2e41cbe63d
shiorioz <ozws011.tmt@gmail.com>
2023-12-04 19:00:36 +0900

    feat: /events/:id/worksでのデータ取得を追加する

--- back/src/routes/events/index.ts
+++ back/src/routes/events/index.ts
@@ -9,6 +9,55 @@ router.get("/", async (req: Request, res: Response) => {
   res.json(events);
 });
 
+// 作品一覧の取得
+router.get("/:id/works", async (req: Request, res: Response) => {
+  // worksからevents_idが一致するものを検索
+  // works_dataのidとworksのlatest_reviewed_idが一致 -> 取得
+  const works = await prisma.works.findMany({
+    where: { events_id: Number(req.params.id) },
+    include: {
+      events: {
+        select: {
+          id: true,
+          name: true,
+          description: true,
+          icon_url: true,
+          start_at: true,
+          end_at: true,
+        },
+      },
+      works_data_works_data_works_idToworks: {
+        select: {
+          name: true,
+          catch_copy: true,
+          works_data_genres: {
+            select: {
+              genres: {
+                select: {
+                  name: true,
+                },
+              },
+            },
+          },
+          works_data_technologies: {
+            select: {
+              technologies: {
+                select: {
+                  name: true,
+                },
+              },
+            },
+          },
+        },
+      },
+    },
+  });
+
+  // TODO: JSONの形式を整形する
+
+  res.json(works);
+});
+
 // イベント参加学生の取得
 router.get("/:id/students", async (req: Request, res: Response) => {
   res.json({ message: "events/{id}/studentsに対するルーティング" });
