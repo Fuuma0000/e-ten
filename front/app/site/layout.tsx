57b3144bfa6a4e282c24201a1919758fbe11d9c9
nonrita <2220025@ecc.ac.jp>
2023-12-05 11:32:42 +0900

    add:events画面を追加しました

---
+++ front/app/site/layout.tsx
@@ -0,0 +1,59 @@
+import { AppBar, Box, Stack, Toolbar, Typography } from "@mui/material";
+
+export default function SiteLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <>
+      <Stack
+        direction="row"
+        sx={{
+          flexGrow: 1,
+          minHeight: "8vh",
+        }}
+      >
+        <Typography
+          component={"h1"}
+          sx={{
+            marginY: "auto",
+            textAlign: "center",
+            width: "25%",
+            fontSize: "1.2rem",
+          }}
+        >
+          サイト名
+        </Typography>
+        <Typography
+          component={"div"}
+          sx={{
+            backgroundColor: "primary.main",
+            width: "75%",
+            height: "8vh",
+            borderRadius: "32px 0px 0px 180px / 0px 0px 0px 150px",
+            borderBottom: "10px solid",
+            borderLeft: "10px solid",
+            borderColor: "tertiary.main",
+            "::before": {
+              content: "''",
+              position: "absolute",
+              top: 0,
+              right: 0,
+              width: "75%",
+              height: "8vh",
+              borderRadius: "32px 0px 0px 180px / 0px 0px 0px 150px",
+              borderBottom: "10px solid",
+              borderLeft: "10px solid",
+              borderColor: "secondary.main",
+            },
+          }}
+        >
+          
+
+        </Typography>
+      </Stack>
+      {children}
+    </>
+  );
+}
