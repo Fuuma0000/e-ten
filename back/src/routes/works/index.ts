b507401b5edf89f1c0cc87f2b3c9f0a05e06ddaa
Fuuma <masa81hiro69@gmail.com>
2024-01-25 13:50:07 +0900

    fix: エンドポイント間違えてた。detailの追加

--- back/src/routes/works/index.ts
+++ back/src/routes/works/index.ts
@@ -80,7 +80,7 @@ router.get("/:id", authenticate, async (req: Request, res: Response) => {
 });
 
 // TODO: 作品の新規作成
-router.post("/works", authenticate, async (req, res) => {
+router.post("/", authenticate, async (req: Request, res: Response) => {
   // クエリパラメータ events_id のデフォルト値を設定
   // イベントに紐づいていない作品を作成する場合は、events_id=0 とする
   const eventsIdQueryParam = (req.query.events_id as string) || "0";
@@ -108,6 +108,7 @@ router.post("/works", authenticate, async (req, res) => {
     movie_url,
     works_url,
     system_diagram_url,
+    detail,
     users,
   } = req.body;
 
@@ -117,84 +118,84 @@ router.post("/works", authenticate, async (req, res) => {
       .status(400)
       .json({ error: "名前または、キャッチコピーが必要です" });
   }
-
-  const transactionResult = await prisma.$transaction(async (prisma) => {
-    const createdWork = await prisma.works.create({
-      data: {
-        events_id: eventId,
-      },
-    });
-
-    const createdWorksData = await prisma.works_data.create({
-      data: {
-        works_id: createdWork.id,
-        name,
-        catch_copy: catch_copy,
-        description,
-        works_url: works_url,
-        movie_url: movie_url,
-        system_diagram_url: system_diagram_url,
-      },
-    });
-
-    // Create Genres
-    await Promise.all(
-      genres_id.map(async (genreId: number) => {
-        await prisma.works_data_genres.create({
-          data: {
-            works_data_id: createdWorksData.id,
-            genres_id: genreId,
-          },
-        });
-      })
-    );
-
-    // Create Technologies
-    await Promise.all(
-      technologies_id.map(async (techId: number) => {
-        await prisma.works_data_technologies.create({
-          data: {
-            works_data_id: createdWorksData.id,
-            technologies_id: techId,
-          },
-        });
-      })
-    );
-
-    // Create Images
-    await Promise.all(
-      images.map(async (imageUrl: string, index: number) => {
-        await prisma.works_data_images.create({
-          data: {
-            works_data_id: createdWorksData.id,
-            url: imageUrl,
-            order: index + 1,
-          },
-        });
-      })
-    );
-
-    // Create Users
-    await Promise.all(
-      users.map(async (user: { user_id: number; role: string }) => {
-        await prisma.works_data_users.create({
-          data: {
-            works_data_id: createdWorksData.id,
-            users_id: user.user_id,
-            role_explanation: user.role,
-          },
-        });
-      })
-    );
-
-    return { createdWork, createdWorksData };
-  });
-
   try {
+    const transactionResult = await prisma.$transaction(async (prisma) => {
+      const createdWork = await prisma.works.create({
+        data: {
+          events_id: eventId,
+        },
+      });
+
+      const createdWorksData = await prisma.works_data.create({
+        data: {
+          works_id: createdWork.id,
+          name,
+          catch_copy: catch_copy,
+          description,
+          works_url: works_url,
+          movie_url: movie_url,
+          system_diagram_url: system_diagram_url,
+          detail: detail,
+        },
+      });
+
+      // Create Genres
+      await Promise.all(
+        genres_id.map(async (genreId: number) => {
+          await prisma.works_data_genres.create({
+            data: {
+              works_data_id: createdWorksData.id,
+              genres_id: genreId,
+            },
+          });
+        })
+      );
+
+      // Create Technologies
+      await Promise.all(
+        technologies_id.map(async (techId: number) => {
+          await prisma.works_data_technologies.create({
+            data: {
+              works_data_id: createdWorksData.id,
+              technologies_id: techId,
+            },
+          });
+        })
+      );
+
+      // Create Images
+      await Promise.all(
+        images.map(async (imageUrl: string, index: number) => {
+          await prisma.works_data_images.create({
+            data: {
+              works_data_id: createdWorksData.id,
+              url: imageUrl,
+              order: index + 1,
+            },
+          });
+        })
+      );
+
+      // Create Users
+      await Promise.all(
+        users.map(async (user: { user_id: number; role: string }) => {
+          await prisma.works_data_users.create({
+            data: {
+              works_data_id: createdWorksData.id,
+              users_id: user.user_id,
+              role_explanation: user.role,
+            },
+          });
+        })
+      );
+
+      return { createdWork, createdWorksData };
+    });
+
     res.status(201).json(transactionResult);
   } catch (error) {
     console.error("作品の投稿中にエラーが起きました:", error);
-    res.status(500).json({ error: "サーバーでエラーが発生しました" });
+    res.status(500).json({ error: "サーバーエラーが発生しました" });
   }
 });
 
